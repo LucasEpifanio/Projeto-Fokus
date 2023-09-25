978d685fb339dae60e330b4c972d3873ed58d574
LucasEpifanio <Lucasepifaniodev@gmail.com>
2023-09-25 18:00:05 +0100

    commit

--- script.js
+++ script.js
@@ -4,20 +4,38 @@ const curtoBtn = document.querySelector('.app__card-button--curto')
 const longoBtn = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+musica.loop = true
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 focoBtn.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBtn.classList.add('active')
 })
 
 curtoBtn.addEventListener('click', () => {
     alterarContexto('descanso-curto')
+    curtoBtn.classList.add('active')
 })
 
 longoBtn.addEventListener('click', () => {
     alterarContexto('descanso-longo')
+    longoBtn.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
